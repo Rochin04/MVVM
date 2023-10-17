c26a132165b99d56723e250d148dc442c46fc9f0
Rochin <emirochi22@gmail.com>
2023-10-16 20:46:37 -0700

    Add project files.

---
+++ MVVM/ViewModel/VMpatron.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM.ViewModel
+{
+    class VMpatron : BaseViewModel
+    {
+        #region CONSTRUCTOR
+        public VMpatron(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #region PROCESOS
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+        public void ProcesoSimple()
+        {
+
+        }
+        #endregion
+        #endregion
+        #region COMANDO
+        public ICommand ProcesoAsyncronocommand => new Command(async () => await ProcesoAsyncrono());
+        public ICommand ProcesoSimCommand => new Command(ProcesoSimple);
+        #endregion
+        
+    }
+}
